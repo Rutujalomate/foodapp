d53ce49dcacbad00dbd6dabd225cfa97e9c4e23a
Rutuja Lomate <104217802+Rutujalomate@users.noreply.github.com>
2022-11-07 19:05:36 +0530

    Add files via upload

---
+++ food order app/components/navbar.js
@@ -0,0 +1,42 @@
+//RealFoodApp/components/navbar.js
+const navbar = () => {
+//     return `        <div id="nav">
+//     <div><a href="./index.html"><img src="./logo2.png" alt="food app"></a></div>
+//     <ul>
+       
+//         <li><a href="wallet.html">Wallet</a></li>
+//         <li><a href="like.html">Favorite</a><sup id=su></sup></li>
+//         <li><a href="cart.html">Cart</a><sup></sup></li>
+//         <li><a href="login.html">Login</a></li>
+//         <li><a href="signup.html">SignUp</a></li>
+//     </ul>
+// </div>`
+
+return `<div id="nav">
+    <div> <img   src="https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-Download-scaled.jpg" alt="" style="width:10%"></div> 
+<ul>
+<li><a href="index.html">Home</a></li>
+
+    <li><a href="wallet.html">wallet</a></li>
+    <li><a href="favourite.html">favourite</li>
+<li> <a href="cart.html">cart</a><sup></sup></li>
+<li><a href="login.html">login</a></li>
+<li><a href="signup.html">signup</a></li>
+
+
+
+
+</ul>
+
+
+        </div>
+        
+   
+        
+        
+        `
+
+
+}
+
+export default navbar;
